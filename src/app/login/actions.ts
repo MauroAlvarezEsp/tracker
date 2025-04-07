e89cbb6d6c3d08481478767817ef3e03977db391
Mauro Alvarez Espinosa <mauro.alvarez@scotiabank.com.mx>
2025-04-07 13:46:34 -0600

    temporal remove login logic

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -7,22 +7,6 @@ import { loginSchema } from "@/utils/utils";
 
 
 export async function login(prevState: any, formData: FormData) {
-  const result = loginSchema.safeParse(Object.fromEntries(formData));
-
-  if (!result.success) 
-    return { errors: result.error.flatten().fieldErrors };
-
-  const { email, password } = result.data;
-  const user: User | undefined = testUsers.find(usr => usr.email == email)
-
-
-  if (!user || user.password !== password) {
-    return {
-      errors: { email: ["Invalid email or password"] },
-    };
-  }
-
-  await createSession(user);
   redirect("/monitor");
 }
 
